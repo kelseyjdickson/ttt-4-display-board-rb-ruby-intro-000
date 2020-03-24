6c61e45c333a2bbdec9f6c55c1633238b8f77bb2
Kelsey Dickson <kelseymedved@gmail.com>
2020-03-24 17:46:05 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,10 +3,10 @@
 board = [" "," "," "," "," "," "," "," "," "]
 
 def display_board(board)
-# puts("   |   |   ")
-# puts("-----------")
-# puts("   |   |   ")
-# puts("-----------")
-# puts("   |   |   ")
-#
-# end
+puts("   |   |   ")
+puts("-----------")
+puts("   |   |   ")
+puts("-----------")
+puts("   |   |   ")
+
+end
